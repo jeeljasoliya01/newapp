c5a726c783e7122cb4b0c0830e9f3dc90ab9abe8
jeeljasoliya01 <101614206+jeeljasoliya01@users.noreply.github.com>
2022-03-24 12:02:05 +0530

    condition

--- src/app/conditional/conditional.component.ts
+++ src/app/conditional/conditional.component.ts
@@ -9,7 +9,10 @@ export class ConditionalComponent implements OnInit {
 
   currselection: string = '';
   selcetionlist: string[] = ['name', 'age', 'image', 'email', 'name_age', 'name_email', 'Show_all'];
- 
+
+  showme: boolean = true;
+  isLoggin: string = '';
+  isPassword: string = '';
   selectedNumber: number = 0;
   showValues() {
     const array = [0];
