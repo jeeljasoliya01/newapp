a1611af0374bbc7550f06c34c125951d941caad1
jeeljasoliya01 <101614206+jeeljasoliya01@users.noreply.github.com>
2022-03-26 13:37:23 +0530

    n

--- src/app/forms-reactive/forms-reactive.component.ts
+++ src/app/forms-reactive/forms-reactive.component.ts
@@ -8,24 +8,94 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormsReactiveComponent implements OnInit {
   userForm: FormGroup;
+  userData: IUser[] = [];
+  selectedHobbies: string[] = [];
+  userHobbies: string[] = ['cricket', 'footboll', 'reading', 'travaling'];
   constructor(private fb: FormBuilder) {
     this.userForm = this.fb.group({
+
+      id: [''],
       firstname: ['', [Validators.required]],
       lastname: ['', [Validators.required]],
       email: ['', [Validators.required,
-                  Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"),
-                  Validators.email]],
-      phone: ['', [Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
-      password:['',[Validators.required,Validators.minLength(8)]],
-      cpassword:['',[Validators.required,Validators.minLength(10)]]
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"),
+      Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      Age: ['', Validators.required],
+      password: ['', [Validators.required, Validators.pattern("((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8})")]],
+      gender: [''],
 
     })
   }
 
   ngOnInit(): void {
+    this.selectedHobbies = ['travaling']
   }
-  submitData(){
-    debugger
+  submitData() {
+    if (!this.userForm.valid) {
+      return;
+    }
+    if (!this.userForm.value.id) {
+      this.userData.push({
+        ...this.userForm.value,
+        id: this.userData.length + 1,
+        hobbies: this.selectedHobbies.join(','),
+      });
+    } else {
+      const isExist = this.userData.find((x) => x.id == this.userForm.value.id);
+      if (isExist) {
+        isExist.firstname = this.userForm.value.firstName;
+        isExist.lastname = this.userForm.value.lastName;
+        isExist.email = this.userForm.value.email;
+        isExist.password = this.userForm.value.password;
+        isExist.Age = this.userForm.value.Age;
+        isExist.phone = this.userForm.value.phone;
+        isExist.gender = this.userForm.value.gender;
+        isExist.hobbies = this.selectedHobbies.join(',');
+      } else {
+        alert('Data not found');
+      }
+    }
   }
-
+  hobbyselected($event: any) {
+    if (this.selectedHobbies.find((x) => x == $event.target.value)) {
+      this.selectedHobbies = this.selectedHobbies.filter(
+        (x) => x != $event.target.value
+      );
+    } else {
+      this.selectedHobbies.push($event.target.value);
+    }
+  }
+}
+getById(id: number){
+  const isExist = this.userData.find((x) => x.id == id);
+  if (isExist) {
+    this.userForm.setvalue({
+      id: isExist.id,
+      firstName: isExist.firstName,
+      lastName: isExist.lastName,
+      email: isExist.email,
+      password: isExist.password,
+      Age: isExist.Age,
+      phone: isExist.phone,
+      gender: isExist.gender,
+    });
+    this.seletedHobbies = isExist.hobbies.split(',');
+  }
+}
+deleteUser(user: IUser){
+  if (confirm('Are you sure you want to delete ?')) {
+    this.userData = this.userData.filter((x) => x.id != user.id);
+  }
+}
+interface IUser {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  Age: string;
+  phone: string;
+  gender: string;
+  hobbies: string;
 }
--- src/app/forms-template/forms-template.component.ts
+++ src/app/forms-template/forms-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,14 +6,32 @@ import { NgForm } from '@angular/forms';
   templateUrl: './forms-template.component.html',
   styleUrls: ['./forms-template.component.css']
 })
-export class FormsTemplateComponent implements OnInit {
-  // @ViewChild('userForm', { static: false }) userForm: NgForm;
+export class FormsTemplateComponent implements OnInit, AfterViewInit {
+
+  selectedHobbies: string[] = [];
+  userHobbies: string[] = ['cricket', 'footboll', 'reading', 'travaling'];
+  
+  @ViewChild('userForm', { static: false }) userForm!: NgForm;
   constructor() { }
 
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit', this.userForm);
+  }
+
   ngOnInit(): void { }
+
 
   submitData(userForm: NgForm) {
     console.log(userForm)
   }
 
+  hobbyselected($event: any) {
+    if (this.selectedHobbies.find((x) => x == $event.target.value)) {
+      this.selectedHobbies = this.selectedHobbies.filter(
+        (x) => x != $event.target.value
+      );
+    } else {
+      this.selectedHobbies.push($event.target.value);
+    }
+  }
 }
